8eaf81b282643067b6257e8f055160a344b52c82
RaishavHanspal <rhanspal25@gmail.com>
2021-06-09 15:57:44 +0530

    lifecycle-hooks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path : 'First', component : FirstComponent},
+{ path : 'Second', component : SecondComponent},
+];
 
 @NgModule({
+  declarations:[
+    FirstComponent,
+    SecondComponent
+  ],
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule, FirstComponent,
+    SecondComponent]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { SimpleService } from './simple.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges,OnInit,DoCheck,AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked, OnDestroy {
   title = 'all-hooks';
+  hooksArray: Array<string> = [];
+  name: string;
+  constructor(private _service:SimpleService){}
+  ngOnChanges(changes){console.log('AppComponent','ngOnChanges', changes); this.hooksArray.push('AppComponent-ngOnChanges');}
+  ngOnInit(){console.log('AppComponent','ngOnInit');  this.hooksArray.push('AppComponent-ngOnInit');}
+  ngDoCheck(){console.log('AppComponent','ngDoCheck');  this.hooksArray.push('AppComponent-ngDoCheck');}
+  ngAfterContentInit(){console.log('AppComponent','ngAfterContentInit');  this.hooksArray.push('AppComponent-ngAfterContentInit');}
+  ngAfterContentChecked(){console.log('AppComponent','ngAfterContentChecked');  this.hooksArray.push('AppComponent-ngAfterContentChecked');}
+  ngAfterViewInit(){console.log('AppComponent','ngAfterViewInit');  this.hooksArray.push('AppComponent-ngAfterViewInit');}
+  ngAfterViewChecked(){console.log('AppComponent','ngAfterViewChecked');  this.hooksArray.push('AppComponent-ngAfterViewChecked');}
+  ngOnDestroy(){console.log('AppComponent','ngOnDestroy');  this.hooksArray.push('AppComponent-ngOnDestroy');}
+  OtherName(event){
+    console.log('event',event)
+    switch (event) {
+      // case "clearAll":
+      //   this.hooksArray = [];
+      //   break;
+      case "hide-DoCheck":
+        this.hooksArray = this.hooksArray.filter(hook => !hook.includes('ngDoCheck'));
+        break;
+      default:
+        this.hooksArray.push(event);
+        break;
+    }
+  }
 }
